12c40d3d1394919cf6073b5e3e52478006982fe8
AlazarBekele <alazarthe70@gmail.com>
2025-11-08 19:28:56 +0300

    Add all urls inside setting url

--- Base/urls.py
+++ Base/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+# Call Urls From all Start apps
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path ('', include('Admin.urls')),
+    path ('clent/', include('Clint.urls')),
+    path ('freelancer/', include('Freelance.urls'))
 ]
