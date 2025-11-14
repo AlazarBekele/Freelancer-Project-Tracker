503f48a67737d7cb95ddf1e8df1ae0e8481b8402
AlazarBekele <alazarthe70@gmail.com>
2025-11-14 20:07:32 +0300

    create auth in view

--- Accounts/admin.py
+++ Accounts/admin.py
@@ -3,7 +3,23 @@ from .models import (
     Welcome,
     AccountTracker
 )
+
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 # Register your models here.
 
 admin.site.register (Welcome)
 admin.site.register (AccountTracker)
+
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ('username', 'email', 'first_name', 'last_name', 'field_choose', 'is_staff')
+    fieldsets = UserAdmin.fieldsets + (
+        ('Custom Fields', {'fields': ('field_choose',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Custom Fields', {'fields': ('field_choose',)}),
+    )
+
+admin.site.register(User, CustomUserAdmin)
